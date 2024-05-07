5f324cc330142ae003ff954d3ba79d193dbf97f1
teep93 <rorryke93@hotmail.com>
2024-05-07 15:53:08 +0800

    add navigate menu function

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ welcome_ascii = """
 username = ''
 password = ''
 account_list_and_balance = {}
-wallet = None
+wallet = 0
+firstname = ''
+lastname = ''
+logged_in = False
 
 
 def logged_out_menu():
@@ -24,32 +27,55 @@ def logged_out_menu():
 
 def logged_in_menu():
     print(welcome_ascii)
+    print(f'Welcome back, {firstname}! What would you like to do today?')
     print('1. View Accounts')
     print('2. Transfer Funds')
     print('3. Open Accounts')
     print('4. Log Out')
 
-def get_choice():
+def navigate_menu(logged_in):
     while True:
-        choice = input('Please enter your choice: ')
-        if choice in ['1', '2', '3']:
-            return choice
+        if logged_in:
+            logged_in_menu()
         else:
-            print('Error. Please enter 1, 2, or 3.')  
+            logged_out_menu()    
+
+        choice = input('Please enter your choice: ')
+
+        if logged_in:
+            if choice == '1':
+                view_accounts()
+            elif choice == '2':
+                transfer_funds()
+            elif choice == '3':
+                open_product_accounts()
+            elif choice == '4':
+                run_logged_out_menu()
+            else:
+                print('Invalid choice. Please enter 1, 2, 3, or 4.')
+        else:
+            if choice == '1':
+                create_user_account()
+            elif choice == '2':
+                log_in()
+            elif choice == '3':
+                exit()
+            else:
+                print('Invalid choice. Please enter 1, 2, or 3.')
+
 
     
 
 def log_in():
     username = input('Enter your username: ')
     password = input('Enter your password: ')
-    for account in created_account:
-        if username == username and password == password:
-            print('Login successful.')
-            self.user_logged_in = True
-            return
-    print('Invalid username or password. Please try again.')    
+    if username == username and password == password:
+        print('Login successful.')
+        logged_in_menu()
+    else:    
+        print('Invalid username or password. Please try again.')    
 
-def exit(self):
+def exit():
     print('Exiting...')
     exit()
 
@@ -57,36 +83,36 @@ def run_logged_out_menu():
     while True:
         logged_out_menu()
         choice = get_choice()
-        if choice == '1':
-            create_account()
-        elif choice == '2':
-            log_in()
-        elif choice == '3':
-            exit()
+
 
 def run_logged_in_menu():
     while True:
+        logged_in_menu()
+        choice = get_choice()
+         
 
 
 
-def create_account():
+def create_user_account():
     print('Create an Account')
     first_name = input('Enter your first name: ')
     last_name = input('Enter your last name: ')
     username = input('Enter desired username: ')
     password = input('Enter desired password: ')
+    print("Account successfully created!")
+    print(f'Name: {first_name} + {last_name}, Username: {username}')
     return (first_name, last_name, username, password)
 
-def display_account_info(self):
+def display_account_info():
     print('Account Information')
-    print(f'First Name: {self.first_name}')
-    print(f'Last Name: {self.last_name}')
-    print(f'Username: {self.username}')
+    print(f'First Name: {first_name}')
+    print(f'Last Name: {last_name}')
+    print(f'Username: {username}')
+    print(f'Account Balances')
 
 
 
 
 
-main_menu = Main()
-main_menu.run_menu()                                   
+run_logged_out_menu()                                 
 
