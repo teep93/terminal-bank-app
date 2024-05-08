793ba404e1319a6616d3347654d6e82af0940169
teep93 <rorryke93@hotmail.com>
2024-05-08 12:42:48 +0800

    import getpass module and add to login function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import getpass
+
+
 welcome_ascii = """
  __       __            __                                                      __                      __    __  __        __          _______                       __       
 |  \  _  |  \          |  \                                                    |  \                    |  \  |  \|  \      |  \        |       \                     |  \      
@@ -10,33 +13,32 @@ welcome_ascii = """
  \$$      \$$  \$$$$$$$ \$$  \$$$$$$$  \$$$$$$  \$$  \$$  \$$  \$$$$$$$          \$$$$   \$$$$$$        \$$   \$$ \$$   \$$ \$$         \$$$$$$$   \$$$$$$$ \$$   \$$ \$$   \$$
 """
 
-printing_seperator = "----------------------------------------------------------------------------------------------"
+printing_separator = "----------------------------------------------------------------------------------------------"
 username = ''
 password = ''
 account_list_and_balance = {'Nhi-Wallet': 1000}
 first_name = ''
 last_name = ''
-full_name = first_name + last_name
 logged_in = False
 
 
 def logged_out_menu():
-    print(printing_seperator)
+    print(printing_separator)
     print(welcome_ascii) 
     print('1. Create a new account')
     print('2. Log in')
     print('3. Exit')
-    print(printing_seperator)
+    print(printing_separator)
 
 def logged_in_menu():
     global first_name, last_name
-    print(printing_seperator)
+    print(printing_separator)
     print(f'Welcome back, {first_name} {last_name}! What would you like to do today?')
     print('1. View Accounts')
     print('2. Transfer Funds')
     print('3. Open Accounts')
     print('4. Log Out')
-    print(printing_seperator)
+    print(printing_separator)
 
 def navigate_menu(logged_in):
     while True:
@@ -55,36 +57,38 @@ def navigate_menu(logged_in):
             elif choice == '3':
                 open_product_accounts()
             elif choice == '4':
-                print(printing_seperator)
+                print(printing_separator)
                 print("Logging out...")
                 print("Successfully logged out.")
-                print(printing_seperator)
+                print(printing_separator)
                 navigate_menu(logged_in=False)
             else:
+                print(printing_separator)
                 print('Invalid choice. Please enter 1, 2, 3, or 4.')
+                print(printing_separator)
         else:
             if choice == '1':
                 create_user_account()
             elif choice == '2':
                 log_in()
             elif choice == '3':
-                print(printing_seperator)
+                print(printing_separator)
                 print("Goodbye!")
                 print("Exiting...")
-                print(printing_seperator)
+                print(printing_separator)
                 exit()
             else:
-                print(printing_seperator)
+                print(printing_separator)
                 print('Invalid choice. Please enter 1, 2, or 3.')
-                print(printing_seperator)
+                print(printing_separator)
 
 
 def view_accounts():
-    if bool(account_list_and_balance):
-        print(printing_seperator)
-        print("Your active accounts and balances")
+        print(printing_separator)
+        print("Your active accounts and balances.")
+        print("All currency in Nhi-Dollars.")
         print(account_list_and_balance)
-        print(printing_seperator)
+        print(printing_separator)
 
 def transfer_funds():
     pass
@@ -96,13 +100,19 @@ def open_product_accounts():
 
 def log_in():
     global username, password
+    print(printing_separator)
     input_username = input('Enter your username: ')
-    input_password = input('Enter your password: ')
+    input_password = getpass.getpass('Enter your password: ')
+    print(printing_separator)
     if input_username == username and input_password == password:
+        print(printing_separator)
         print('Login successful.')
+        print(printing_separator)
         navigate_menu(logged_in=True)
-    else:    
-        print('Invalid username or password. Please try again.')    
+    else:
+        print(printing_separator)    
+        print('Invalid username or password. Please try again.') 
+        print(printing_separator)   
 
 
 
@@ -117,10 +127,11 @@ def create_user_account():
     last_name = input('Enter your last name: ')
     username = input('Enter desired username: ')
     password = input('Enter desired password: ')
-    print(printing_seperator)
+    print(printing_separator)
     print("Account successfully created!")
-    print(f'Name: {first_name} {last_name}, Username: {username}')
-    print(printing_seperator)
+    print(f'Name: {first_name} {last_name}')
+    print(f'Username: {username}')
+    print(printing_separator)
     return first_name, last_name, username, password
 
 def display_account_info():
